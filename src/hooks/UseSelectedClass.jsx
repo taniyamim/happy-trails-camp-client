eba586ec49571e9fa46d9985a2041264645151f5
taniyamim <[jannatulhafsa500@gmail.com]>
2023-06-09 03:16:28 +0600

    token edited

--- src/hooks/UseSelectedClass.jsx
+++ src/hooks/UseSelectedClass.jsx
@@ -1,14 +1,18 @@
 import React, { useContext } from 'react';
 import { useQuery } from '@tanstack/react-query'
 import { AuthContext } from '../Providers/AuthProvider';
+import useAxiosSecure from './useAxiosSecure';
 
 const UseSelectedClass = () => {
+    
     const { user } = useContext(AuthContext);
+    // const token = localStorage.getItem('access-token');
+    const [axiosSecure] = useAxiosSecure();
     const {refetch, data: selectedClass =[]} = useQuery({
         queryKey: ['selectedClass' , user?.email],
         queryFn: async ()=>{
-            const res =await fetch(`http://localhost:5000/selectedClasses?email=${user.email}`)
-            return res.json();
+            const res =await axiosSecure(`/selectedClasses?email=${user.email}`)
+            return res.data;
         },
     })
     return [selectedClass , refetch]
