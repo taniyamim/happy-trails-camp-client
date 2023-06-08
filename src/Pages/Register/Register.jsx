6a8c26e868bf633a4cc5dd42f8d53e445d8fb047
taniyamim <[jannatulhafsa500@gmail.com]>
2023-06-08 13:06:59 +0600

    react-form-hook added

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,13 +1,124 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import Swal from 'sweetalert2';
 import { AuthContext } from '../../Providers/AuthProvider';
-import { useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 const Register = () => {
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+    const { createUser, signInWithGoogle } = useContext(AuthContext);
+    const onSubmit = data => {
+
+        createUser(data.email, data.password)
+            .then(result => {
+
+                const loggedUser = result.user;
+                console.log(loggedUser);
+
+                updateUserProfile(data.name, data.photoURL)
+                    .then(() => {
+                        const saveUser = { name: data.name, email: data.email }
+                        fetch('https://bistro-boss-server-fawn.vercel.app/users', {
+                            method: 'POST',
+                            headers: {
+                                'content-type': 'application/json'
+                            },
+                            body: JSON.stringify(saveUser)
+                        })
+                            .then(res => res.json())
+                            .then(data => {
+                                if (data.insertedId) {
+                                    reset();
+                                    Swal.fire({
+                                        position: 'top-end',
+                                        icon: 'success',
+                                        title: 'User created successfully.',
+                                        showConfirmButton: false,
+                                        timer: 1500
+                                    });
+                                    navigate('/');
+                                }
+                            })
+
+
+
+                    })
+                    .catch(error => console.log(error))
+            })
+    };
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    console.log('signup page location', location)
+    const from = location.state?.from?.pathname || '/'
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
 
 
     return (
-        <div></div>
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row">
+                <div className="w-1/3 mr-12">
+                    {/* <img src={logo} alt="" className="rounded-lg" /> */}
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+                        <h1 className="text-3xl text-center font-bold">Register</h1>
+                        <div >
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" name='name' placeholder="Name" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name='email' placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Photo Url</span>
+                                </label>
+                                <input type="text" name='photo' placeholder="photo url" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" />
+
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Confirm Password</span>
+                                </label>
+                                <input type="password" name='confirmPassword' placeholder="Retype your password" className="input input-bordered" />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+
+
+                            <div className="form-control mt-6">
+                                <input className="btn bg-pink-900 font-extrabold text-white" type="submit" value="Sign Up" />
+                            </div>
+                        </div>
+                        <p className='my-4 text-center'>Already an user? <Link className='text-pink-400 font-bold' to="/login">Login</Link> </p>
+                        <div className="divider">OR</div>
+                        <button className="btn gap-2 bg-pink-900 text-white">
+                            Sign In With Google
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-google" viewBox="0 0 16 16"> <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" /> </svg>
+                        </button>
+
+                        <p className="text-red-600">{error}</p>
+                        <p className="text-green-600">{success}</p>
+
+                    </form>
+                </div>
+            </div>
+        </div>
     );
 };
 
