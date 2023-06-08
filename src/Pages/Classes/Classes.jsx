57bf3dd045e7b9b230d6238ca232ee8315de87aa
taniyamim <[jannatulhafsa500@gmail.com]>
2023-06-08 11:31:42 +0600

    dynamic title added

--- src/Pages/Classes/Classes.jsx
+++ src/Pages/Classes/Classes.jsx
@@ -1,41 +1,41 @@
 import React, { useEffect, useState } from 'react';
-
+import { Helmet } from 'react-helmet-async';
 const Classes = () => {
     const [classes, setClasses] = useState([]);
     useEffect(() => {
         fetch('data.json')
             .then(res => res.json())
             .then(data => {
-
                 setClasses(data);
                 console.log(data);
             });
     }, []);
+
     return (
         <div>
+            <Helmet>
+                <title>
+                Happy Trails Camp | Classes
+                </title>
+            </Helmet>
             <div className=''>
                 <h1 className='text-4xl font-extrabold border-t-2 border-b-2 text-white text-center py-5'>Classes</h1>
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 my-8'>
-
-                {
-                    classes.map(cls => <div>
-                        <div className="card w-96 glass">
-                            <figure><img src={cls.image} className='h-52 py-5 rounded-xl' alt="car!" /></figure>
-                            <div className="card-body">
-                                <h2 className="card-title">{cls.name}</h2>
-                                <p> <span className='font-bold'>Instructor:</span> {cls.instructorName}</p>
-                                <p> <span className='font-bold'>Available Seats:</span> {cls.availableSeats}</p>
-                                <p> <span className='font-bold'>Price:</span> ${cls.price}</p>
-                                <div className="card-actions justify-end">
-                                    <button className="btn text-white bg-rose-900">Enrol now!</button>
-                                </div>
+                {classes.map(cls => (
+                    <div key={cls.id} className={`card w-96 glass ${cls.availableSeats === 0 ?  'bg-red-900 text-white' : ''}`}>
+                        <figure><img src={cls.image} className='h-52 py-5 rounded-xl' alt="car!" /></figure>
+                        <div className="card-body">
+                            <h2 className="card-title">{cls.name}</h2>
+                            <p> <span className='font-bold'>Instructor:</span> {cls.instructorName}</p>
+                            <p> <span className='font-bold'>Available Seats:</span> {cls.availableSeats}</p>
+                            <p> <span className='font-bold'>Price:</span> ${cls.price}</p>
+                            <div className="card-actions justify-end">
+                                <button className="btn text-white bg-rose-900">Enroll now!</button>
                             </div>
                         </div>
-                    </div>)
-                }
-
-
+                    </div>
+                ))}
             </div>
         </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,16 @@ import {
 } from "react-router-dom";
 import { router } from './Routes/Router';
 import AuthProvider from './Providers/AuthProvider';
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <div className='max-w-screen-xl mx-auto'>
-      <RouterProvider router={router} />
-      </div>
-    </AuthProvider>
+    <HelmetProvider>
+      <AuthProvider>
+        <div className='max-w-screen-xl mx-auto'>
+          <RouterProvider router={router} />
+        </div>
+      </AuthProvider>
+    </HelmetProvider>
   </React.StrictMode>,
 )
